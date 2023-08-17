95c4e6bddd23b7bef03e37556df3efeedc4cd4e5
LeonardiLaura <73897108+LeonardiLaura@users.noreply.github.com>
2023-08-17 22:22:37 +0200

    add change room to map

--- src/test/scala/Model/GameMap/GameMapSpec.scala
+++ src/test/scala/Model/GameMap/GameMapSpec.scala
@@ -1,12 +1,12 @@
 package Model.GameMap
 
-import Exceptions.RoomNotFoundException
-import Model.Room.{Room, RoomBuilder}
-import Model.Cells.Position
+import Exceptions.{LinkNotFoundException, RoomNotFoundException}
+import Model.Room.{Room, RoomBuilder, RoomLink}
+import Model.Cells.{Direction, Position}
 import org.scalatest.BeforeAndAfterEach
 import org.scalatest.matchers.should.Matchers.*
 import org.scalatest.flatspec.AnyFlatSpec
-import org.scalatest.TryValues._
+import org.scalatest.TryValues.*
 import Model.TestUtils.*
 
 class GameMapSpec extends AnyFlatSpec with BeforeAndAfterEach:
@@ -34,11 +34,40 @@ class GameMapSpec extends AnyFlatSpec with BeforeAndAfterEach:
   "A map" should "retrieve a room having its name" in {
     map.getRoomFromName(roomName).isSuccess should be(true)
     map.getRoomFromName(roomName).success.value should be(room)
-    // map.getRoomFromName("pippo") should
   }
 
   "A map" should "retrieve an error if the name is not present when trying to get a room" in {
     val wrongName: String = "pippo"
     map.getRoomFromName(wrongName).isFailure should be(true)
     map.getRoomFromName(wrongName).failure.exception shouldBe a[RoomNotFoundException]
+  }
+
+  "A map" should "retrieve the info to get in the next room" in {
+    val pos2: Position = (1, 1)
+    val name2: String = "room2"
+    val room2: Room = new Room(name2, Set.empty, Set(RoomLink(pos2, genericDirection, roomName, defaultPosition)))
+    map = new GameMap(mapName, Set(room, room2), roomName, defaultPosition)
+    map.changeRoom(pos2, name2, genericDirection).isSuccess should be(true)
+    map.changeRoom(pos2, name2, genericDirection).success.value should be(room, defaultPosition)
+  }
+
+  "A map" should "retrieve an error if the info to get in the next room are not present" in {
+    val wrongName: String = "pippo"
+    // start room not present
+    map.changeRoom(defaultPosition, wrongName, genericDirection).isFailure should be(true)
+    map.changeRoom(defaultPosition, wrongName, genericDirection).failure.exception shouldBe a[RoomNotFoundException]
+    // start room without the link
+    map.changeRoom(defaultPosition, roomName, genericDirection).isFailure should be(true)
+    map.changeRoom(defaultPosition, roomName, genericDirection).failure.exception shouldBe a[LinkNotFoundException]
+    // start room with the link but it has the wrong direction
+    room = new Room(roomName, Set.empty, Set(RoomLink(defaultPosition, genericDirection, wrongName, (1, 1))))
+    map = new GameMap(mapName, Set(room), roomName, defaultPosition)
+    map.changeRoom(defaultPosition, roomName, genericDirection.opposite).isFailure should be(true)
+    map
+      .changeRoom(defaultPosition, roomName, genericDirection.opposite)
+      .failure
+      .exception shouldBe a[LinkNotFoundException]
+    // room with the link but the destination room in the link doesn't exist
+    map.changeRoom(defaultPosition, roomName, genericDirection).isFailure should be(true)
+    map.changeRoom(defaultPosition, roomName, genericDirection).failure.exception shouldBe a[RoomNotFoundException]
   }
