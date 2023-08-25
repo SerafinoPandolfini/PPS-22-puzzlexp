8ce245b9c43ebf42b93030146cc4baaf4e4b9eca
Serafino Pandolfini <serafino.pandolfini@studio.unibo.it>
2023-08-25 11:51:19 +0200

    add javadoc

--- src/main/scala/EngineProlog/PrologConverter.scala
+++ src/main/scala/EngineProlog/PrologConverter.scala
@@ -8,9 +8,14 @@ import alice.tuprolog.{Struct, Term, Theory}
 object PrologConverter:
   val Separator = ","
 
+  /**
+   * convert a [[cell]] into its prolog representation: c(cellName,position_x,position_y,{additional proprieties})
+   * @param cell the cell to convert
+   * @param properties eventual additional proprieties to add to the [[Cell]] representation, default add nothing
+   * @return the prolog representation of the cell
+   */
   def convert(cell: Cell, properties: Cell => String = _ => ""): String =
     val cellString = Room.cellToString(cell, true).toLowerCase
     val positionString = s"${cell.position._1}$Separator${cell.position._2}"
     val propertiesString = properties(cell)
-
     s"c($cellString$Separator$positionString$propertiesString)"
