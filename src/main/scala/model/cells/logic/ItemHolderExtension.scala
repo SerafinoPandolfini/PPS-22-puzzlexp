c3ba34c0682cb046f8aaf32290a42b7660dd5e68
Sofy24 <sofia.tosi4@studio.unibo.it>
2023-08-31 18:14:40 +0200

    implemented itemHolder extension methods and DSL

---
+++ src/main/scala/model/cells/logic/ItemHolderExtension.scala
@@ -0,0 +1,48 @@
+package model.cells.logic
+
+import model.cells.Item
+import model.room.ItemHolder
+
+import scala.annotation.targetName
+
+object ItemHolderExtension:
+  /** extension for adding new methods for the [[ItemHolder]]
+    */
+  extension (itemHolder: ItemHolder)
+    /** Alias for [[ItemHolder.addItems()]] */
+    @targetName("addItemsAlias")
+    def ++(items: List[Item]): ItemHolder = itemHolder.addItems(items)
+
+    /** Alias for [[ItemHolder.removeItem()]] */
+
+    @targetName("removeItemAlias")
+    def -(item: Item): ItemHolder = itemHolder.removeItem(item)
+
+    /** Alias for [[ItemHolder.isPresent()]] */
+    @targetName("isPresentAlias")
+    def ?(item: Item): Boolean = itemHolder.isPresent(item)
+
+    /** map every [[Cell]] type to a two char string
+      *
+      * @param item
+      *   the item to map
+      * @return
+      *   the mapped item
+      */
+    def stringedItem(item: Item): String =
+      item match
+        case i if i == Item.Box  => "BOX"
+        case i if i == Item.Axe  => "AXE"
+        case i if i == Item.Key  => "KEY"
+        case i if i == Item.Pick => "PICK"
+        case _                   => "??"
+
+    /** return a visual representation of [[ItemHolder.itemOwned]]
+      *
+      * @param mapper
+      *   the mapper to transform the items in the corresponding string
+      * @return
+      *   the string with the items in [[ItemHolder.itemOwned]]
+      */
+    def itemHolderToString(mapper: Item => String): String =
+      "[ " + itemHolder.itemOwned.map(item => mapper(item)).mkString(" | ") + " ]"
--- src/test/scala/model/room/ItemHolderSpec.scala
+++ src/test/scala/model/room/ItemHolderSpec.scala
@@ -4,6 +4,7 @@ import model.cells.Item
 import org.scalatest.BeforeAndAfterEach
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers.*
+import model.cells.logic.ItemHolderExtension.*
 
 class ItemHolderSpec extends AnyFlatSpec with BeforeAndAfterEach:
 
@@ -15,22 +16,36 @@ class ItemHolderSpec extends AnyFlatSpec with BeforeAndAfterEach:
 
   "an item holder" should "be able to add items" in {
     itemHolder.itemOwned should be(List())
+    itemHolder.itemHolderToString(itemHolder.stringedItem) should be("[  ]")
     itemHolder = itemHolder.addItems(List(Item.Pick))
     itemHolder.itemOwned should be(List(Item.Pick))
+    itemHolder.itemHolderToString(itemHolder.stringedItem) should be("[ PICK ]")
   }
 
   "an item holder" should "be able to say if an item is present or not" in {
     itemHolder.itemOwned should be(List())
+    itemHolder.itemHolderToString(itemHolder.stringedItem) should be("[  ]")
     itemHolder = itemHolder.addItems(List(Item.Axe))
     itemHolder.isPresent(Item.Pick) should be(false)
     itemHolder.isPresent(Item.Axe) should be(true)
+    itemHolder.itemHolderToString(itemHolder.stringedItem) should be("[ AXE ]")
   }
 
   "an item holder" should "be able to remove an item" in {
     itemHolder.itemOwned should be(List())
+    itemHolder.itemHolderToString(itemHolder.stringedItem) should be("[  ]")
     itemHolder = itemHolder.addItems(List(Item.Axe))
     itemHolder = itemHolder.addItems(List(Item.Key))
     itemHolder.isPresent(Item.Key) should be(true)
     itemHolder = itemHolder.removeItem(Item.Key)
-    itemHolder.isPresent(Item.Key) should be(false)
+    itemHolder ? Item.Key should be(false)
+    itemHolder.itemHolderToString(itemHolder.stringedItem) should be("[ AXE ]")
+  }
+
+  "an item holder" should "be able to show its items" in {
+    itemHolder
+      ++ List(Item.Axe, Item.Key, Item.Pick)
+      - Item.Key itemHolderToString itemHolder.stringedItem should be(
+        "[ AXE | PICK ]"
+      )
   }
