984623355eb57e05b59aa2508ede61309d89a02f
Sofy24 <sofia.tosi4@studio.unibo.it>
2023-09-11 15:38:15 +0200

    fixed two tests for Linux versions

--- src/test/scala/menu/MenuSpec.scala
+++ src/test/scala/menu/MenuSpec.scala
@@ -4,15 +4,26 @@ import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers.*
 import org.scalatest.BeforeAndAfterEach
 
+import java.awt.GraphicsEnvironment
+
 class MenuSpec extends AnyFlatSpec with BeforeAndAfterEach:
-  super.beforeEach()
+
+  override def beforeEach(): Unit =
+    if !GraphicsEnvironment.isHeadless then MenuController.start()
+    super.beforeEach()
+
+  override def afterEach(): Unit =
+    super.afterEach()
+    if !GraphicsEnvironment.isHeadless then MenuController.view.dispose()
 
   "a controller menu" should "be able to get all the path of the map file" in {
-    MenuController.start()
-    MenuController.mapPathAndName.map((p, _) => p) shouldBe a[List[String]]
+    if !GraphicsEnvironment.isHeadless then
+      MenuController.start()
+      MenuController.mapPathAndName.map((p, _) => p) shouldBe a[List[String]]
   }
 
   "a controller menu" should "be able to get all the name of the map" in {
-    MenuController.start()
-    MenuController.mapPathAndName.map((_, n) => n) shouldBe a[List[String]]
+    if !GraphicsEnvironment.isHeadless then
+      MenuController.start()
+      MenuController.mapPathAndName.map((_, n) => n) shouldBe a[List[String]]
   }
