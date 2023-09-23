69c1ee96b2edfbef68ab4a0f2439386247221521
Serafino Pandolfini <serafino.pandolfini@studio.unibo.it>
2023-09-23 11:39:20 +0200

    refactor and javadoc

--- src/main/scala/model/gameMap/Minimap.scala
+++ src/main/scala/model/gameMap/Minimap.scala
@@ -6,12 +6,16 @@ import model.gameMap.GameMap
 import model.room.Room
 import utils.extensions.PositionExtension.{+, -}
 
+/** the minimap of the [[Room]]s of the played [[GameMap]]
+  */
 object Minimap:
 
   extension (map: GameMap)
+    /** create the associated minimap as a list of [[MinimapElement]]s
+      */
     def createMinimap(): List[MinimapElement] =
       val (rooms, initialRoom) = map.rooms.partition(_.name != map.initialRoom)
-      val minimapElements = extendMapping(initialRoom.head, rooms.toList, (0, 0))
+      val minimapElements = extendMapping(initialRoom.head, rooms.toList)
       val correctionValue = standardizePosition(minimapElements)
       fillGaps(
         minimapElements
@@ -19,9 +23,18 @@ object Minimap:
           .toList
       ).sorted(MinimapOrdering)
 
+  /** the order of the minimap
+   * @return an ordered [[List]] of [[MinimapElement]] for rows
+   */
   given MinimapOrdering: Ordering[MinimapElement] = Ordering.by(e => (e.position._2, e.position._1))
 
-  private def extendMapping(room: Room, rooms: List[Room], position: Position): Set[MinimapElement] =
+  /** produces a transposition of the [[Room]]s into [[MinimapElement]]s
+   * @param room the [[GameMap.initialRoom]] where the player starts
+   * @param rooms all the other [[Room]]s in [[GameMap]]
+   * @param position the starting [[Position]] for the mapping
+   * @return the partial [[Minimap]]
+   */
+  private def extendMapping(room: Room, rooms: List[Room], position: Position = (0, 0)): Set[MinimapElement] =
     val (connectedRooms, unmappedRooms) = rooms.partition(r => room.links.map(link => link.toRoom).contains(r.name))
     val mappedRooms = for
       roomsToMap <- connectedRooms
@@ -34,11 +47,19 @@ object Minimap:
     yield mappedRoom
     mappedRooms.toSet + MinimapElement(room.name, position, room.links.map(_.direction).toSet, false, true)
 
+  /** search the minimum values of the mapping for fixing [[MinimapElement]]s' [[Position]]s
+   * @param minimapElements the elements of the minimap
+   * @return the value to subtract to all [[MinimapElement]]s
+   */
   private def standardizePosition(minimapElements: Iterable[MinimapElement]): (Int, Int) =
     minimapElements.foldLeft((0, 0)) { case ((minX, minY), element) =>
       (Math.min(minX, element.position._1), Math.min(minY, element.position._2))
     }
 
+  /** search for missing [[MinimapElement]]s and produces placeholders for filling the gaps
+   * @param minimap the incomplete minimap 
+   * @return the new minimap completed with placeholders
+   */
   private def fillGaps(minimap: List[MinimapElement]): List[MinimapElement] =
     val (maxX, maxY): Position = minimap.foldLeft((Int.MinValue, Int.MinValue)) { case ((maxX, maxY), element) =>
       (math.max(maxX, element.position._1), math.max(maxY, element.position._2))
--- src/main/scala/model/gameMap/MinimapElement.scala
+++ src/main/scala/model/gameMap/MinimapElement.scala
@@ -5,6 +5,14 @@ import model.cells.properties.Direction
 
 import scala.collection.immutable.Set
 
+/**
+ * the element that compose the [[Minimap]]
+ * @param name the name of the corresponding [[model.room.Room]]
+ * @param position the position inside the minimap
+ * @param directions the transposition of the [[model.room.RoomLink]]
+ * @param visited if the associated room has been visited by the player
+ * @param existing if the associated room is real or a placeholder
+ */
 case class MinimapElement(
     name: String,
     position: Position,
