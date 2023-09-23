2aa33d5cebacb6d881375631ab6c910fd95372a2
Sofy24 <sofia.tosi4@studio.unibo.it>
2023-09-24 01:04:19 +0200

    fixed a bug

--- src/test/scala/view/PathExtractorSpec.scala
+++ src/test/scala/view/PathExtractorSpec.scala
@@ -14,13 +14,13 @@ class PathExtractorSpec extends AnyFlatSpec:
 
   "from a cell it" should "be able to extract a path for its respective image" in {
     val cellsWithPaths: List[(Cell, String)] = List(
-      (BasicCell(position1_1), "cell_BS_W"),
-      (WallCell(position2_1), "cell_WL"),
-      (HoleCell(position1_2), "cell_HL_W_D"),
-      (CliffCell(position2_2, Item.Empty, Direction.Left), "cell_CL_LEFT"),
-      (CoveredHoleCell(position3_2), "cell_CH_W_C"),
-      (ButtonCell(position3_1, color = Color.Red), "cell_BT_W_RED"),
-      (ButtonCell(position3_3, color = Color.Blue, pressableState = PressableState.Pressed), "cell_BT_W_BLUE_P")
+      (BasicCell(position1_1), "cell_BS_W_C"),
+      (WallCell(position2_1), "cell_WL_C"),
+      (HoleCell(position1_2), "cell_HL_W_D_C"),
+      (CliffCell(position2_2, Item.Empty, Direction.Left), "cell_CL_LEFT_C"),
+      (CoveredHoleCell(position3_2), "cell_CH_W_C_C"),
+      (ButtonCell(position3_1, color = Color.Red), "cell_BT_W_RED_C"),
+      (ButtonCell(position3_3, color = Color.Blue, pressableState = PressableState.Pressed), "cell_BT_W_BLUE_P_C")
     )
     cellsWithPaths.foreach((c, s) => extractPath(c) should be(s))
   }
@@ -51,5 +51,5 @@ class PathExtractorSpec extends AnyFlatSpec:
       CoveredHoleCell(position3_2),
       ButtonCell(position3_3, color = Color.Red)
     )
-    extractPath(WallCell(position2_2), cellsWithPaths) should be("cell_WL_NW")
+    extractPath(WallCell(position2_2), cellsWithPaths) should be("cell_WL_SE")
   }
