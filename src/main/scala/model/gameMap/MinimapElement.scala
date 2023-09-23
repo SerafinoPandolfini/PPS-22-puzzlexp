9d5643dd5a85a170b14ddf5b36fc28e1702f6fcc
Serafino Pandolfini <serafino.pandolfini@studio.unibo.it>
2023-09-23 11:57:21 +0200

    add the possiblity to visit a MinimapElement

--- src/main/scala/model/gameMap/MinimapElement.scala
+++ src/main/scala/model/gameMap/MinimapElement.scala
@@ -5,18 +5,29 @@ import model.cells.properties.Direction
 
 import scala.collection.immutable.Set
 
-/**
- * the element that compose the [[Minimap]]
- * @param name the name of the corresponding [[model.room.Room]]
- * @param position the position inside the minimap
- * @param directions the transposition of the [[model.room.RoomLink]]
- * @param visited if the associated room has been visited by the player
- * @param existing if the associated room is real or a placeholder
- */
+/** the element that compose the [[Minimap]]
+  * @param name
+  *   the name of the corresponding [[model.room.Room]]
+  * @param position
+  *   the position inside the minimap
+  * @param directions
+  *   the transposition of the [[model.room.RoomLink]]
+  * @param visited
+  *   if the associated room has been visited by the player
+  * @param existing
+  *   if the associated room is real or a placeholder
+  */
 case class MinimapElement(
     name: String,
     position: Position,
     directions: Set[Direction],
     visited: Boolean,
     existing: Boolean
-)
+):
+
+  /** visit the MinimapElement if it exist
+   * @return a visited [[MinimapElement]]
+   */
+  def visit(): MinimapElement =
+    if existing then copy(visited = true)
+    else copy()
---
+++ src/test/scala/model/gameMap/MinimapElementSpec.scala
@@ -0,0 +1,27 @@
+package model.gameMap
+
+import model.cells.properties.Direction
+import org.scalatest.{BeforeAndAfterEach, GivenWhenThen}
+import org.scalatest.matchers.should.Matchers.*
+import org.scalatest.flatspec.AnyFlatSpec
+import utils.TestUtils.{defaultPosition, position1_1}
+
+class MinimapElementSpec extends AnyFlatSpec with BeforeAndAfterEach with GivenWhenThen:
+
+  var visitableElement: MinimapElement = _
+  var placeholderElement: MinimapElement = _
+
+  override def beforeEach(): Unit =
+    visitableElement = MinimapElement("visitable", defaultPosition, Set.empty[Direction], false, true)
+    placeholderElement = MinimapElement("placeholder", position1_1, Set.empty[Direction], false, false)
+
+  "A MinimapElement" should "be visitable if it exist" in {
+    Given("a visitable element")
+    When("visited by the player")
+    Then("it should be visited")
+    visitableElement.visit().visited should be(true)
+    Given("a placeholder element")
+    When("visited by the player")
+    Then("it should not be visited")
+    placeholderElement.visit().visited should not be true
+  }
