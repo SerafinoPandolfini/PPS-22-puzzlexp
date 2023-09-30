34669e02b9336b4bb2369ae014e3119b4ba89cd1
Sofy24 <sofia.tosi4@studio.unibo.it>
2023-09-30 18:34:22 +0200

    refactored a name

--- src/test/scala/model/cells/LockSpec.scala
+++ src/test/scala/model/cells/LockSpec.scala
@@ -8,43 +8,43 @@ import model.cells.logic.CellExtension.updateItem
 import model.cells.properties.{Item, WalkableType}
 
 class LockSpec extends AnyFlatSpec with BeforeAndAfterEach:
-  var doorCell: LockCell = _
+  var lockCell: LockCell = _
 
   override def beforeEach(): Unit =
     super.beforeEach()
-    doorCell = LockCell(DefaultPosition)
+    lockCell = LockCell(DefaultPosition)
 
-  "A door cell" should "be close" in {
-    doorCell.open should be(false)
+  "A lock cell" should "be close" in {
+    lockCell.open should be(false)
   }
 
-  "A door cell" should "be opened with a key" in {
-    var cells: Set[Cell] = Set(doorCell)
-    doorCell.open should be(false)
-    cells = doorCell.updateItem(cells, Item.Axe, GenericDirection)
-    doorCell = cells.collectFirst { case cell: LockCell => cell }.get
-    doorCell.open should be(false)
-    cells = doorCell.updateItem(cells, Item.Key, GenericDirection)
-    doorCell = cells.collectFirst { case cell: LockCell => cell }.get
-    doorCell.open should be(true)
+  "A lock cell" should "be opened with a key" in {
+    var cells: Set[Cell] = Set(lockCell)
+    lockCell.open should be(false)
+    cells = lockCell.updateItem(cells, Item.Axe, GenericDirection)
+    lockCell = cells.collectFirst { case cell: LockCell => cell }.get
+    lockCell.open should be(false)
+    cells = lockCell.updateItem(cells, Item.Key, GenericDirection)
+    lockCell = cells.collectFirst { case cell: LockCell => cell }.get
+    lockCell.open should be(true)
   }
 
-  "a opened door" should "not be closed" in {
-    var cells: Set[Cell] = Set(doorCell)
-    cells = doorCell.updateItem(cells, Item.Key, GenericDirection)
-    doorCell = cells.collectFirst { case cell: LockCell => cell }.get
-    doorCell.open should be(true)
-    cells = doorCell.updateItem(cells, Item.Key, GenericDirection)
-    doorCell = cells.collectFirst { case cell: LockCell => cell }.get
-    doorCell.open should be(true)
+  "a opened lock" should "not be closed" in {
+    var cells: Set[Cell] = Set(lockCell)
+    cells = lockCell.updateItem(cells, Item.Key, GenericDirection)
+    lockCell = cells.collectFirst { case cell: LockCell => cell }.get
+    lockCell.open should be(true)
+    cells = lockCell.updateItem(cells, Item.Key, GenericDirection)
+    lockCell = cells.collectFirst { case cell: LockCell => cell }.get
+    lockCell.open should be(true)
   }
 
-  "only a opened door" should "be walkable" in {
-    var cells: Set[Cell] = Set(doorCell)
-    doorCell.open should be(false)
-    doorCell.walkableState should be(WalkableType.Walkable(false))
-    cells = doorCell.updateItem(cells, Item.Key, GenericDirection)
-    doorCell = cells.collectFirst { case cell: LockCell => cell }.get
-    doorCell.open should be(true)
-    doorCell.walkableState should be(WalkableType.Walkable(true))
+  "only a opened lock" should "be walkable" in {
+    var cells: Set[Cell] = Set(lockCell)
+    lockCell.open should be(false)
+    lockCell.walkableState should be(WalkableType.Walkable(false))
+    cells = lockCell.updateItem(cells, Item.Key, GenericDirection)
+    lockCell = cells.collectFirst { case cell: LockCell => cell }.get
+    lockCell.open should be(true)
+    lockCell.walkableState should be(WalkableType.Walkable(true))
   }
